706006b41499eb5f666d887e7683eee42d4a48ce
s-subeen <101384641+s-subeen@users.noreply.github.com>
2023-11-07 20:48:25 +0900

    Delete 배열의 평균값.kt

--- Kotlin/배열의 평균값.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun solution(numbers: IntArray): Double {
-        var sum: Double = 0.0
-        for (item in numbers) {
-            sum += item
-        }
-        return sum / numbers.size
-    }
-}
