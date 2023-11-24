caee5a31eb3934b1d4323b9822c77cc2d4bdacc5
s-subeen <101384641+s-subeen@users.noreply.github.com>
2023-11-24 12:47:58 +0900

    Add: Programmers-Algorithm

---
+++ Kotlin/lv2/피보나치 수/피보나치 수.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var array = IntArray(n + 1){1}
+        var answer = 0
+        
+        for (i in 3..n) {
+            array[i] = (array[i - 1] + array[i - 2]) % 1234567
+        }
+        
+        return array[n]
+    }
+}
